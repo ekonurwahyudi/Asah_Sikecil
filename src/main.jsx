6d887f93556788e641b121c34c077b3c9eae23d6
ekonurwahyudi <generasi@gmail.com>
2025-07-28 22:56:12 +0700

    roolback bismillah

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom' // Ganti BrowserRouter dengan HashRouter
 import './index.css'
 import App from './App.jsx'
 import FreeOfferPage from './components/FreeOfferPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter> {/* Ganti BrowserRouter dengan HashRouter */}
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/free-offer" element={<FreeOfferPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
